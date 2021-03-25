054fb31f6da318811b0249dbfed7a65e83f3dcf0
francois-stjacques <60405331+francois-stjacques@users.noreply.github.com>
2021-03-25 13:26:35 -0400

    configuration du point b

--- src/services/config.js
+++ src/services/config.js
@@ -5,7 +5,17 @@
 
 // Configuration Firebase (mettez-y les vôtres !)
 const firebaseConfig = {
-  
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBozBJDhErsze0IJgygOky2hplt-rhhlB8",
+  authDomain: "panier-achat-fsj.firebaseapp.com",
+  databaseURL: "https://panier-achat-fsj-default-rtdb.firebaseio.com",
+  projectId: "panier-achat-fsj",
+  storageBucket: "panier-achat-fsj.appspot.com",
+  messagingSenderId: "827452650180",
+  appId: "1:827452650180:web:7c8928380ddc7d8a844f91",
+  measurementId: "G-7SJNXY6SXX"
+};
 };
 export default firebaseConfig;
 
